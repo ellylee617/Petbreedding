151b1a57128b2991aa4b5e7413d5a8d00e3f163e
shkim <qudwls7042@gmail.com>
2021-06-11 15:14:35 +0900

    update login.jsp, join.jsp, login.css, join.css, reset.css
    add login.js

---
+++ Petbreedding/src/main/webapp/resources/js/member/login.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+
+$(function() {
+	$("#searchIdPw").click(function() {
+		$("#modal").show();
+	});
+
+	$("#modalExit").click(function() {
+		$('.searchModal_Container').hide();
+	});
+});
