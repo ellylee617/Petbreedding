828313d5690a0268837983eb8be6ebeae955eec8
elly <ellylee617@gmail.com>
2021-06-15 01:39:57 +0900

    21.06.15 리뷰관리 페이지 매핑 추가

--- Petbreedding/src/main/java/kh/com/petbreedding/BP/controller/BPController.java
+++ Petbreedding/src/main/java/kh/com/petbreedding/BP/controller/BPController.java
@@ -9,48 +9,53 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class BPController {
-	
-	//  사장님회원가입 페이지로 이동
+
+	// 사장님회원가입 페이지로 이동
 	@RequestMapping(value = "/bJoin", method = RequestMethod.GET)
 	public String bJoin(Locale locale, Model model) {
-		
+
 		// TODO Auto-generated method stub
 		return "/member/bJoin";
 	}
-	
+
 	// 사장님 승인 대기 페이지로 이동
 	@RequestMapping(value = "/bJoinSuccess", method = RequestMethod.GET)
 	public String bjoinSuccess(Locale locale, Model model) {
 		return "/member/bJoinSuccess";
-		
+
 	}
-	
+
 	// 사장님 제휴문의 대기 페이지로 이동
 	@RequestMapping(value = "/bWait", method = RequestMethod.GET)
 	public String bWait(Locale locale, Model model) {
 		return "/member/bWait";
-		
+
 	}
-	
+
 	// 사장님 제휴문의 대기 페이지로 이동
 	@RequestMapping(value = "/bModify", method = RequestMethod.GET)
 	public String bModify(Locale locale, Model model) {
 		return "/member/bModify";
-		
+
 	}
-	
+
 	// 울트라콜 상품 조회
 	@RequestMapping(value = "/cta", method = RequestMethod.GET)
 	public String cta(Locale locale, Model model) {
 		return "/member/cta";
-		
+
 	}
-	
+
 	// 사장님 업체 정보 페이지로 이동
-		@RequestMapping(value = "/bShop", method = RequestMethod.GET)
-		public String bShop(Locale locale, Model model) {
-			return "/shop/bShopInfo";
-			
-		}
+	@RequestMapping(value = "/bShop", method = RequestMethod.GET)
+	public String bShop(Locale locale, Model model) {
+		return "/shop/bShopInfo";
+	}
+
+	// 업체 리뷰 관리 페이지로 이동
+	@RequestMapping(value = "/bReview", method = RequestMethod.GET)
+	public String bReview(Locale locale, Model model) {
+		return "/shop/bReview";
+	}
 
 }
