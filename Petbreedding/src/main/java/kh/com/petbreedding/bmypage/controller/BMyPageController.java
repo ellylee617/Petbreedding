fb216a48c25342ce35f76900579727320ffad34e
haeniyu <19hn97@gmail.com>
2021-06-14 15:10:52 +0900

    사업자 - 마이페이지 - 내정보 수정 1. bMyPageUpdate, bMyPageAside jsp, css 추가 2. kh.com.petbreedding.bmypage.controller.BMyPageController 에    사장님 마이 페이지 내정보 수정 컨트롤러 추가 (@RequestMapping(value = "/bMyPageUpdate"))

---
+++ Petbreedding/src/main/java/kh/com/petbreedding/bmypage/controller/BMyPageController.java
@@ -0,0 +1,21 @@
+package kh.com.petbreedding.bmypage.controller;
+
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BMyPageController {
+	
+	// 사장님 마이 페이지 내정보 수정
+	@RequestMapping(value = "/bMyPageUpdate", method = RequestMethod.GET)
+	public String bMyPageUpdate(Locale locale, Model model) {
+		
+		// TODO Auto-generated method stub
+		return "/myPage/bMyPageUpdate";
+}
+
+}
---
+++ Petbreedding/src/main/webapp/WEB-INF/views/myPage/bMyPageAside.jsp
@@ -0,0 +1,25 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<aside class="bMyPageAside">
+	<div class="divBolder">
+		<br>
+			<label>마이페이지</label>
+		<ul>
+			<br>
+			<a href="#"><li>내정보 수정</li></a>
+			<br>
+			<br>
+		</ul>
+			<label>고객센터</label>
+		<ul>
+			<br>
+			<a href="#"><li>공지사항</li></a>
+			<a href="#"><li>자주묻는 질문</li></a>
+			<a href="#"><li>내 문의 내역</li></a>
+			<a href="#"><li>1:1 문의하기</li></a>
+			<br>
+			<br>
+		</ul>
+		<a href="#"><label>제휴 취소 신청</label></a>
+	</div>
+</aside>
