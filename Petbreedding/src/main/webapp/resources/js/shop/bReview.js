4e7cd6cb3f68959fbdbcaf05ee61a8d51da45a52
elly <ellylee617@gmail.com>
2021-06-15 15:20:00 +0900

    21.06.15 사업자 - 리뷰 관리 페이지 js 수정 / 관리자 - 사업장 관리 페이지 js 생성

--- Petbreedding/src/main/webapp/resources/js/shop/bReview.js
+++ Petbreedding/src/main/webapp/resources/js/shop/bReview.js
@@ -1,9 +1,11 @@
 var modal = document.getElementById("modalReply");
 var modalReWin = document.getElementById("modalReply-Win");
-var btnReply = document.querySelector(".btnReply")
-btnReply.addEventListener("click", function() {
-	modal.style.display = "flex";
-});
+var btnReply = document.querySelectorAll(".btnReply");
+for (var i = 0; i < btnReply.length; i++) {
+	btnReply[i].addEventListener("click", function() {
+		modal.style.display = "flex";
+	});
+}
 
 var closeBtn = modal.querySelector(".modal_close_btn")
 closeBtn.addEventListener("click", function() {
