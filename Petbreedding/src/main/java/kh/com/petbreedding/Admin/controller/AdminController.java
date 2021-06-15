a1d0141cdbeb4aaacd83c3edf4a9f16012a49aa3
elly <ellylee617@gmail.com>
2021-06-15 19:41:00 +0900

    21.06.15 매핑 수정

--- Petbreedding/src/main/java/kh/com/petbreedding/Admin/controller/AdminController.java
+++ Petbreedding/src/main/java/kh/com/petbreedding/Admin/controller/AdminController.java
@@ -13,49 +13,50 @@ public class AdminController {
 	// 사업장관리 - 제휴 승인 대기 페이지로 이동
 	@RequestMapping(value = "/mwait", method = RequestMethod.GET)
 	public String mWait(Locale locale, Model model) {
-		return "/shop/mWaitforPartner";
+		return "/admin/aShop/mWaitforPartner";
 	}
-	
-	// 사업장관리 - 제휴 취소 대기 페이지로 이동
-		@RequestMapping(value = "/mcancel", method = RequestMethod.GET)
-		public String mCancel(Locale locale, Model model) {
-			return "/shop/mCancelPartner";
-		}
 
-		//게시글 관리 (문의게시판 목록)
+	// 사업장관리 - 제휴 취소 대기 페이지로 이동
+	@RequestMapping(value = "/mcancel", method = RequestMethod.GET)
+	public String mCancel(Locale locale, Model model) {
+		return "/admin/aShop/mCancelPartner";
+	}
+
+	// 게시글 관리 (문의게시판 목록)
 	@RequestMapping(value = "/mboard", method = RequestMethod.GET)
 	public String mboard(Locale locale, Model model) {
-		return "/myPage/mboard";
+		return "/admin/aBoard/mboard";
 	}
-	//게시글 관리 (문의게시판 글상세) 
+
+	// 게시글 관리 (문의게시판 글상세)
 	@RequestMapping(value = "/mboardcon", method = RequestMethod.GET)
 	public String mboardcon(Locale locale, Model model) {
-		return "/myPage/mboardcon";
+		return "/admin/aBoard/mboardcon";
 	}
-	
-	//게시글 관리 (공지사항게시판 목록) 
-		@RequestMapping(value = "/mservice", method = RequestMethod.GET)
-		public String mservice(Locale locale, Model model) {
-			return "/myPage/mservice";
-		}
 
-		// 게시글 관리 (공지사항게시판 내용)
-		@RequestMapping(value = "/mservicecon", method = RequestMethod.GET)
-		public String mservicecon(Locale locale, Model model) {
-			return "/myPage/mservicecon";
-		}
-		
-		//게시글 관리 (자유게시판 목록) 
-		@RequestMapping(value = "/mfreeboard", method = RequestMethod.GET)
-		public String mfreeboard(Locale locale, Model model) {
-				return "/myPage/mfreeboard";
-			}
-		
-		//게시글 관리 (자유게시판 내용) 
-				@RequestMapping(value = "/mfreecon", method = RequestMethod.GET)
-				public String mfreecon(Locale locale, Model model) {
-						return "/myPage/mfreecon";
-					}
-	
+	// 게시글 관리 (공지사항게시판 목록)
+	@RequestMapping(value = "/mservice", method = RequestMethod.GET)
+	public String mservice(Locale locale, Model model) {
+		return "/admin/aBoard/mservice";
+	}
+
+	// 게시글 관리 (공지사항게시판 내용)
+	@RequestMapping(value = "/mservicecon", method = RequestMethod.GET)
+	public String mservicecon(Locale locale, Model model) {
+		return "/admin/aBoard/mservicecon";
+	}
+
+	// 게시글 관리 (자유게시판 목록)
+	@RequestMapping(value = "/mfreeboard", method = RequestMethod.GET)
+	public String mfreeboard(Locale locale, Model model) {
+		return "/admin/aBoard/mfreeboard";
+	}
+
+	// 게시글 관리 (자유게시판 내용)
+	// ?? 해당 파일이 없어요?
+	@RequestMapping(value = "/mfreecon", method = RequestMethod.GET)
+	public String mfreecon(Locale locale, Model model) {
+		return "/admin/aBoard/mfreecon";
+	}
 
 }
