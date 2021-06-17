0677b0679f6f6cc3d0f00b8ef53f2f70731eb6d6
haeniyu <19hn97@gmail.com>
2021-06-17 14:45:03 +0900

    CtaController 오류나는 코드 삭제

--- Petbreedding/src/main/java/kh/com/petbreedding/cta/controller/CtaController.java
+++ Petbreedding/src/main/java/kh/com/petbreedding/cta/controller/CtaController.java
@@ -17,8 +17,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.siot.IamportRestClient.response.Payment;
-
 import kh.com.petbreedding.cta.model.service.CtaService;
 import kh.com.petbreedding.cta.model.vo.Cta;
 import kh.com.petbreedding.cta.model.vo.CtaPay;
