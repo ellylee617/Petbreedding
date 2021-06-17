963a186e8d9bb90341be86b9c4ed4394ccc95d40
yeonee-dev <yeonn1229@naver.com>
2021-06-17 14:07:41 +0900

    울트라콜 vo

--- Petbreedding/src/main/java/kh/com/petbreedding/cta/controller/CtaController.java
+++ Petbreedding/src/main/java/kh/com/petbreedding/cta/controller/CtaController.java
@@ -17,9 +17,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-/*import com.siot.IamportRestClient.response.Payment;*/
-
-
+import com.siot.IamportRestClient.response.Payment;
 
 import kh.com.petbreedding.cta.model.service.CtaService;
 import kh.com.petbreedding.cta.model.vo.Cta;
