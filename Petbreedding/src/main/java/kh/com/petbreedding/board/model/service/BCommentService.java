4d690540048b7a3c3677f8f8f9da51b65914a43b
shkim <qudwls7042@gmail.com>
2021-07-02 00:49:48 +0900

    자유게시판 댓글 다오, 서비스 설정 완료

---
+++ Petbreedding/src/main/java/kh/com/petbreedding/board/model/service/BCommentService.java
@@ -0,0 +1,17 @@
+package kh.com.petbreedding.board.model.service;
+
+import java.util.List;
+
+import kh.com.petbreedding.board.model.vo.B_comment;
+
+public interface BCommentService {
+	public List<B_comment> bCommentSelectListA();
+	
+	public List<B_comment> bCommentSelectListC(String bo_num);
+	
+	public String getbCommentSeq();
+	
+	public int bCommentInsert(B_comment bComment);
+	
+	public int bocChkUpdate(String bo_num);
+}
