32d8efe4d0d994a7b0879236b21e72fbbc66fa70
haeniyu <19hn97@gmail.com>
2021-07-02 12:28:52 +0900

    사업장 수정 페이지 - 동물병원 주휴일 표시 완료

--- Petbreedding/src/main/java/kh/com/petbreedding/bmypage/model/vo/Hospital.java
+++ Petbreedding/src/main/java/kh/com/petbreedding/bmypage/model/vo/Hospital.java
@@ -18,14 +18,21 @@ public class Hospital {
 //	private String hosClick;	// 동물병원 잔여 클릭수
 	private String shopMImg; // 동물병원 대표 사진 파일명
 	
+	private String mon;
+	private String tue;
+	private String wed;
+	private String thu;
+	private String fri;
+	private String sat;
+	private String sun;
 	
-
 
 	@Override
 	public String toString() {
 		return "Hospital [hosNum=" + hosNum + ", bpId=" + bpId + ", shopName=" + shopName + ", shopAddr=" + shopAddr
 				+ ", y=" + y + ", x=" + x + ", shopTel=" + shopTel + ", shopTime=" + shopTime + ", shopMInfo="
-				+ shopMInfo + ", shopMImg=" + shopMImg + "]";
+				+ shopMInfo + ", shopMImg=" + shopMImg + ", mon=" + mon + ", tue=" + tue + ", wed=" + wed + ", thu="
+				+ thu + ", fri=" + fri + ", sat=" + sat + ", sun=" + sun + "]";
 	}
 
 
@@ -113,6 +120,76 @@ public class Hospital {
 
 	public void setShopMImg(String shopMImg) {
 		this.shopMImg = shopMImg;
+	}
+
+
+	public String getMon() {
+		return mon;
+	}
+
+
+	public void setMon(String mon) {
+		this.mon = mon;
+	}
+
+
+	public String getTue() {
+		return tue;
+	}
+
+
+	public void setTue(String tue) {
+		this.tue = tue;
+	}
+
+
+	public String getWed() {
+		return wed;
+	}
+
+
+	public void setWed(String wed) {
+		this.wed = wed;
+	}
+
+
+	public String getThu() {
+		return thu;
+	}
+
+
+	public void setThu(String thu) {
+		this.thu = thu;
+	}
+
+
+	public String getFri() {
+		return fri;
+	}
+
+
+	public void setFri(String fri) {
+		this.fri = fri;
+	}
+
+
+	public String getSat() {
+		return sat;
+	}
+
+
+	public void setSat(String sat) {
+		this.sat = sat;
+	}
+
+
+	public String getSun() {
+		return sun;
+	}
+
+
+	public void setSun(String sun) {
+		this.sun = sun;
 	}
 
 	
