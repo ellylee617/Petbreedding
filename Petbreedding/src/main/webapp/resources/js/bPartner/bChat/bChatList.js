658ad8091289aaa26f7dd0935cbda5b9f8eac2bc
elly <ellylee617@gmail.com>
2021-07-05 16:30:46 +0900

    21.07.05 채팅 삭제를 위한 js, css 수정

--- Petbreedding/src/main/webapp/resources/js/bPartner/bChat/bChatList.js
+++ Petbreedding/src/main/webapp/resources/js/bPartner/bChat/bChatList.js
@@ -13,37 +13,39 @@ function openbchatwin(inchatId,innickName, inshopName, inclNum) {
 
 }
 
-
-//삭제 버튼 누르면 모달창 띄움
-$(".cancleM").on("click", function(e) {
-	$("#cancel-modal").css("display", "block");
-	e.stopPropagation();
-});
-
-$("#noCalncle").on("click", function() {
-	$("#cancel-modal").css("display", "none");
-});
-
-$("#yesCalncle").on("click", function() {
-	var bp_Id = $("#bp_Id").val();
-
-	$.ajax({
-		url : "bDeleteDo",
-		type : "POST",
-		data : {
-			bp_Id : bp_Id
-		},
-		success : function(data) {
-			if (data > 0) {
-			} else {
+function deleteChat(inchatId) {
+	event.stopPropagation();
+	
+	var chatId = inchatId;
+	
+	$("#deleteMsg-modal").css("display", "block");
+	
+	// 취소를 누르면 취소
+	$("#noDelete").on("click", function() {
+		$("#deleteMsg-modal").css("display", "none");
+	});
+	
+	// 확인을 누르면 삭제(로 보이게) 진행
+	$("#yesDelete").on("click", function() {
+		$.ajax({
+			url : "bchatdelete.do",
+			type : "POST",
+			data : { chatId, chatId },
+			success : function(data) {
+				if(data>0){
+					alert("대화가 모두 삭제되었습니다!");
+				}else{
+					alert("오류가 발생했습니다. 관리자에게 문의해주세요.");
+				}
+			},
+			error : function(error) {
 				alert("오류가 발생했습니다. 관리자에게 문의해주세요.");
 			}
-		},
-		error : function(error) {
-			alert("오류가 발생했습니다. 관리자에게 문의해주세요.");
-		}
+		});
+		$("#deleteMsg-modal").css("display", "none");
+		setTimeout("location.reload()", 300);
 	});
-});
+}
 
 $(document).ready(function() {
 	// 1분에 한번씩 새로고침으로 신규 채팅 체크
