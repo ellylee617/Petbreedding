07dad2ac2b35acad7220b949888fe165ad3aa888
yeonee-dev <yeonn1229@naver.com>
2021-07-13 22:05:07 +0900

    결제하게 alert 삭제

--- Petbreedding/src/main/webapp/resources/js/bPartner/bSales/ctaPay2.js
+++ Petbreedding/src/main/webapp/resources/js/bPartner/bSales/ctaPay2.js
@@ -69,10 +69,6 @@ m_redirect_url: 'successCta'
 console.log(rsp);
 if (rsp.success) {
 var msg = '결제가 완료되었습니다.';
-msg += '고유ID : ' + rsp.imp_uid;
-msg += '상점 거래ID : ' + rsp.merchant_uid;
-msg += '결제 금액 : ' + rsp.paid_amount;
-msg += '카드 승인번호 : ' + rsp.apply_num;
 location.href = 'successCta';
 paysuceess();
 
@@ -81,7 +77,6 @@ paysuceess();
 var msg = '결제에 실패하였습니다.';
 msg += '에러내용 : ' + rsp.error_msg;
 }
-alert(msg);
 
 
 
